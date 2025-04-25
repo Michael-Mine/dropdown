be0f06f134691fbf06115f07e857ea91866d5f80
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-04-25 10:41:34 +0100

    Add module to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function dropdown(button, dropdownDiv) {
+  const buttonToDropdown = document.querySelector(button);
+  const dropdownButtonsDiv = document.querySelector(dropdownDiv);
+
+  dropdownButtonsDiv.setAttribute("style", "visibility: hidden");
+
+  buttonToDropdown.addEventListener("click", () => {
+    dropdownButtonsDiv.setAttribute("style", "visibility: visible");
+  });
+
+  buttonToDropdown.addEventListener("mouseover", () => {
+    dropdownButtonsDiv.setAttribute("style", "visibility: visible");
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!event.target.closest(dropdownDiv)) {
+      dropdownButtonsDiv.setAttribute("style", "visibility: hidden");
+    }
+  });
+}
+
+module.exports = dropdown;
